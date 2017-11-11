b9e4a0f2c351fe9c6aaa6c7b4b819b757466bac4
mohan <lmohan717@gmail.com>
2017-11-11 23:20:12 +0530

    db errors configurations added

--- website/settings.py
+++ website/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 #         'PORT': '',
 #     }
 # }
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation
