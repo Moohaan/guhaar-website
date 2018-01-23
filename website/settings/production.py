01a4f5a07ffda73704c25dea0350661503a67698
mohan <lmohan717@gmail.com>
2018-01-23 23:11:58 +0530

    settings changed

--- website/settings/production.py
+++ website/settings/production.py
@@ -2,8 +2,8 @@ from website.settings.base import *
 
 import dj_database_url
 
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
