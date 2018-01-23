7ede0216cad886f85d99f3cea01428777875acdb
mohan <lmohan717@gmail.com>
2018-01-23 23:08:12 +0530

    settings changed

--- website/settings/production.py
+++ website/settings/production.py
@@ -4,8 +4,11 @@ import dj_database_url
 
 DEBUG = False
 # DEBUG = True
-
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ALLOWED_HOSTS = ['guhaarwebsite.herokuapp.com', 'guhaar.com', 'www.guhaar.com', '']
 
 DATABASES = { 'default': dj_database_url.config() }
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
