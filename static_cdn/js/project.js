87d1136c60fbf650d98f8df635fccc56ab956e24
mohan <lmohan717@gmail.com>
2018-02-05 18:27:06 +0530

    ajax requests have been minimized heavely in .js file

--- static_cdn/js/project.js
+++ static_cdn/js/project.js
@@ -9,10 +9,12 @@ $( document ).ready(function() {
   var closeModalSelector = $( ".close_modal" );
 
   // Hide and Show project modal ***STARTS***
-  var hideTime = 200;
-  var offSet = 130;
-  var scrollTime = 500;
-  var scrollBackId = 0;
+  var hideTime = 200; // used for animating effect
+  var offSet = 130; // offset from the top if the body: USED WHILE SCROLLING THE HTML BODY
+  var scrollTime = 900;
+  var scrollBackId = 0; // Keeping track of where was user when click was triggered to get the user back to the same location on the page
+  
+  var fetchedObjects = {}; // To keep track of which item is fetched and which is not in order to minimize the Ajax calls
 
   // Navbar clicks STARTS
   // scrollToStoriesSelector.on('click', function(event) {
@@ -24,65 +26,100 @@ $( document ).ready(function() {
   // });
   // Navbar clicks ENDS
 
-function getDetails(url,str){
+function getAjaxResults(url,str,id){
 
   $.ajax({
        url: url,
        dataType: "json",
+
        success: function(data){
           var json = JSON.parse(data);
-          populateModal(json,str);
+          console.log('ajax called!');
+          populateModal(str,json);
+          fetchedObjects[id]= json;
        },
+
        error: function (data) {
          console.log(data);
          alert('Sorry, try again.');
        }
+
    });
+
 }
 
 // project click handling **STARTS**
 
 function insertDataInDom(parentSelector, htmlData){
+  // populate the particular dom elements
   parentSelector.find('div.modal_details>div:first-of-type').html('');
   parentSelector.find('div.modal_details>div:first-of-type').html(htmlData);
+
 }
 
-function populateModal(json,str){
+function populateModal(str,json){
   var data;
-  if(str=="projects"){
-    data = JSON.parse(json.project)[0].fields;
-    src = "http://res.cloudinary.com/guhaar/"+data.image;
-    alt = data.title;
-    insertDataInDom(modalSelector,'<img src='+src+' alt='+alt+'>');
+
+  if(str=="members"){
+     // If User clicked the team members' images
+      src = "http://res.cloudinary.com/guhaar/"+member.image;
+      name =  member.name;
+      short_description = member.short_description;
+      description = member.description;
+      str = '<img src='+src+' alt="member thumbnail"><h2>'+name+'</h2><p>'+short_description+'</p>';
+
+      insertDataInDom(modalSelector, str);
+      modalSelector.find('.member_details p').html(member.description);
+
+  }else {
+
+      if(str=="projects"){
+          data = JSON.parse(json.project)[0].fields;
+          src = "http://res.cloudinary.com/guhaar/"+data.image;
+          alt = data.title;
+          insertDataInDom(modalSelector,'<img src='+src+' alt='+alt+'>');
+      }else{
+          data = JSON.parse(json.data)[0].fields;
+
+          if(data.url){
+            insertDataInDom(modalSelector, data.url);
+
+          }else{
+            src = "http://res.cloudinary.com/guhaar/"+data.image;
+            alt = data.title;
+            insertDataInDom(modalSelector,'<img src='+src+' alt='+alt+'>');
+          }
+      }
+      modalSelector.find('h2').html(data.title);
+      modalSelector.find('.modal_details p').html(data.description);
   }
-  else{
-    data = JSON.parse(json.data)[0].fields;
-    if(data.url){
-      insertDataInDom(modalSelector, data.url);
-    }else{
-      src = "http://res.cloudinary.com/guhaar/"+data.image;
-      alt = data.title;
-      insertDataInDom(modalSelector,'<img src='+src+' alt='+alt+'>');
-    }
-  }
-  modalSelector.find('h2').html(data.title);
-  modalSelector.find('.modal_details p').html(data.description);
 }
 
 objectSelector.on( "click", '.object_details', function( event ) {
       event.preventDefault();
       closeModalSelector.parent().removeClass("hide");
       closeModalSelector.parent().slideDown(hideTime);
+
       var parentSelector = $(this).parents('.object');
       var id = parentSelector.attr('id');
-      var str = parentSelector.attr('object-type');
+      var str = parentSelector.attr('object-type'); // assign the item type i.e. Project, Video or interview
       var url = "/"+str+"/"+id+"/";
+
       scrollBackId = id;
-      console.log(url);
-      getDetails(url,str);
+      // console.log(url);
+      if(fetchedObjects[id]){
+        // to check is this item has been fetched before
+        console.log('ajax not called');
+        populateModal(str,fetchedObjects[id]);
+      } else {
+        // If not then make ajax call
+        getAjaxResults(url,str,id);
+      }
+
       $('html, body').animate({
         scrollTop: $(parentSelector.parents('.objects')).offset().top-offSet
       }, scrollTime);
+
   });
   // project click handling ** ENDS **
 
@@ -91,25 +128,32 @@ memberSelector.on( "click", function( event ) {
       event.preventDefault();
       closeModalSelector.parent().removeClass("hide");
       closeModalSelector.parent().slideDown(hideTime);
+
       var id = $(this).attr('id');
       scrollBackId = id;
-      $.ajax({
-           url: "/aboutus/"+id+"/",
-           dataType: "json",
-           success: function(data){
-            member = JSON.parse(data)[0].fields;
-            src = "http://res.cloudinary.com/guhaar/"+member.image;
-            name =  member.name;
-            short_description = member.short_description;
-            description = member.description;
-            str = '<img src='+src+' alt="member thumbnail"><h2>'+name+'</h2><p>'+short_description+'</p>';
-            insertDataInDom(modalSelector, str);
-            modalSelector.find('.member_details p').html(member.description);
-           },
-           error: function (data) {
-             alert('unexpected Error, try again.');
-           }
-       });
+      if(fetchedObjects[id]){
+        // to check is this item has been fetched before
+        console.log('ajax not called');
+        populateModal("members",fetchedObjects[id]);
+
+      } else {
+        // If not then make ajax call
+          $.ajax({
+               url: "/aboutus/"+id+"/",
+               dataType: "json",
+
+               success: function(data){
+                  member = JSON.parse(data)[0].fields;
+                  fetchedObjects[id] = member;
+                  populateModal("members",fetchedObjects[id]);
+               },
+
+               error: function (data) {
+                 alert('unexpected Error, try again.');
+               }
+           });
+         }
+
        $('html, body').animate({
          scrollTop: $(memberSelector.parent('.team')).offset().top-offSet
        }, scrollTime);
@@ -119,13 +163,16 @@ memberSelector.on( "click", function( event ) {
   closeModalSelector.on( "click", function( event ) {
       event.preventDefault();
       $(this).parent().slideUp(hideTime);
+
       insertDataInDom(modalSelector, '<img src="" alt="">');
       modalSelector.find('h2').html('');
       modalSelector.find('.modal_details p').html('');
-      console.log(".object#"+scrollBackId);
+      // console.log(".object#"+scrollBackId);
+
       $('html, body').animate({
-        scrollTop: $(".object#"+scrollBackId).offset().top
+        scrollTop: $(".object#"+scrollBackId).offset().top-offSet
       }, scrollTime);
+
   });
 
 // Hide and Show project modal  ***ENDS***
