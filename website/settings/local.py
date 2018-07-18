775a5e7eacb7855aee98b327f4bcffdbbf536e13
Mohan <lmohan717@gmail.com>
2018-07-18 22:49:03 +0530

    e3ferwf

--- website/settings/local.py
+++ website/settings/local.py
@@ -1,5 +1,4 @@
 from website.settings.base import *
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 cloudinary.config(
   cloud_name = os.environ.get('cloud_name'),
@@ -8,8 +7,3 @@ cloudinary.config(
 )
 DEBUG = True
 SECURE_SSL_REDIRECT = False
-
-# UPLOADCARE = {
-#     'pub_key': 'd054d38a036a4221f387',
-#     'secret': 'e106e526a2b3c02586af ',
-# }
--- website/settings/production.py
+++ website/settings/production.py
@@ -2,20 +2,14 @@ from website.settings.base import *
 
 import dj_database_url
 # import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+# import cloudinary.uploader
+# import cloudinary.api
 
 # DEBUG = False
 DEBUG = True
 SECURE_SSL_REDIRECT = False
 
-ALLOWED_HOSTS = ['guhaarwebsite.herokuapp.com', 'guhaar.com', 'www.guhaar.com']
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = 'j*u0%km0j4n(ldq3*1yry)bhg%qt=66up$x_zjbx$r_t4_!6l&'
-
 DATABASES = { 'default': dj_database_url.config() }
-
 
 # # Cloudinary settings using python code. Run before pycloudinary is used.
 cloudinary.config(
